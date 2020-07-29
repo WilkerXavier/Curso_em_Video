94548b5109b0976db361bda2869105401fa8cbb9
Wilker Xavier <willcostaxavier@hotmail.com>
2020-07-29 20:46:13 -0300

    imprime o Dobro, Triplo, Raiz,  do numero digitado

---
+++ 006DobroTriploRaizQuadrada.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+dobro = numero * 2 
+triplo = numero * 3
+raiz = numero ** (1/2)
+
+print("O dobro do numero digitado é: {} ".format(dobro))
+print("O triplo do numero digitado é: {} ".format(triplo))
+print("A raiz quadrada de {} é: {} ".format(numero, raiz))
