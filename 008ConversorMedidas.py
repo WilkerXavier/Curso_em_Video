f0b43fe6b43dbd3a372d4725047e068dc41bf817
Wilker Xavier <willcostaxavier@hotmail.com>
2020-07-31 11:34:36 -0300

    Conever para centimetros e milimetros

---
+++ 008ConversorMedidas.py
@@ -0,0 +1,6 @@
+medida = float(input("Uma distância em metros: "))
+centimetro = medida * 100
+milimetros = medida * 1000
+
+print("A medida de {} metros, em centimetros é: {:.0f}".format(medida, centimetro))
+print("A medida de {} metros, em milimetros é: {:.0f}".format(medida, milimetros))
